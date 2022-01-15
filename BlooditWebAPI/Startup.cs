f35354fabdbce945c5bee9539113aa0ef496c3f9
Nikita Kovalenko <nikkv.ottf@gmail.com>
2022-01-15 22:37:59 +0200

    added packages for graphql webapi

--- BlooditWebAPI/Startup.cs
+++ BlooditWebAPI/Startup.cs
@@ -23,9 +23,9 @@ namespace BlooditWebAPI
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddGraphQLServer();
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
@@ -34,7 +34,6 @@ namespace BlooditWebAPI
             });
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
