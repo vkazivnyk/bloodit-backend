6e39767796a1995d499aeae93d03eb0e9390cbf0
Max <rendon4ik@gmail.com>
2022-01-17 18:50:12 +0200

    Add more resolvers for gql types

--- BlooditWebAPI/GraphQL/Comments/CommentType.cs
+++ BlooditWebAPI/GraphQL/Comments/CommentType.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using BlooditData.Models;
+using BlooditData.Repositories;
+using HotChocolate;
 using HotChocolate.Types;
 
 namespace BlooditWebAPI.GraphQL.Comments
@@ -13,7 +15,28 @@ namespace BlooditWebAPI.GraphQL.Comments
         {
             descriptor.Description("Represents the type for a post comment.");
 
+            descriptor
+                .Field(c => c.User)
+                .ResolveWith<Resolvers>(r => r.GetUser(default!, default!));
+
+            descriptor
+                .Field(c => c.Post)
+                .ResolveWith<Resolvers>(r => r.GetPost(default!, default!));
+
             base.Configure(descriptor);
+        }
+
+        private class Resolvers
+        {
+            public ApplicationUser GetUser(Comment comment, [Service] IAppRepository repository)
+            {
+                return repository.GetUserByCommentId(comment.Id);
+            }
+
+            public Post GetPost(Comment comment, [Service] IAppRepository repository)
+            {
+                return repository.GetPostByCommentId(comment.Id);
+            }
         }
     }
 }
--- BlooditWebAPI/GraphQL/Posts/PostType.cs
+++ BlooditWebAPI/GraphQL/Posts/PostType.cs
@@ -15,7 +15,37 @@ namespace BlooditWebAPI.GraphQL.Posts
         {
             descriptor.Description("Represents the type for a user post.");
 
+            descriptor
+                .Field(p => p.Comments)
+                .ResolveWith<Resolvers>(r => r.GetComment(default!, default!));
+
+            descriptor
+                .Field(p => p.User)
+                .ResolveWith<Resolvers>(r => r.GetUser(default!, default!));
+
+            descriptor
+                .Field(p => p.Topic)
+                .ResolveWith<Resolvers>(r => r.GetTopic(default!, default!));
+
             base.Configure(descriptor);
+        }
+
+        private class Resolvers
+        {
+            public IEnumerable<Comment> GetComment(Post post, [Service] IAppRepository repository)
+            {
+                return repository.GetCommentsByPostId(post.Id);
+            }
+
+            public ApplicationUser GetUser(Post post, [Service] IAppRepository repository)
+            {
+                return repository.GetUserByPostId(post.Id);
+            }
+
+            public Topic GetTopic(Post post, [Service] IAppRepository repository)
+            {
+                return repository.GetTopicByPostId(post.Id);
+            }
         }
     }
 }
