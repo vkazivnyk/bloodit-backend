9d51939f1dec9a1177e74641eaf908e8cc9749c9
Nikita Kovalenko <nikkv.ottf@gmail.com>
2022-01-17 14:09:01 +0200

    added ApplicationUser extensions to implement Topics field

--- BlooditWebAPI/GraphQL/Query.cs
+++ BlooditWebAPI/GraphQL/Query.cs
@@ -11,6 +11,11 @@ namespace BlooditWebAPI.GraphQL
     [GraphQLDescription("Represents user, topic, post & comment queries.")]
     public class Query
     {
+        public IEnumerable<ApplicationUser> GetUser([Service] IAppRepository repository)
+        {
+            return repository.GetUsers();
+        }
+
         [GraphQLDescription("Represents the query for retrieving posts.")]
         public IEnumerable<Post> GetPost([Service] IAppRepository repository)
         {
